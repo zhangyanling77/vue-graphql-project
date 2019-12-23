10287d442f5bfd8c318049b84a32da879c1caf60
zhangyanling77 <18783226594@163.com>
2019-12-23 16:02:10 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-use-before-define": "off",
+    "no-unused-vars": "off",
+    "import/prefer-default-export": "off"
   },
   parserOptions: {
     parser: 'babel-eslint',
