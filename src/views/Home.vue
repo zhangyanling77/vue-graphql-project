6300dd5192f5994093431b4bdf5fec54eaf68355
zhangyanling77 <18783226594@163.com>
2019-12-23 16:11:50 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,58 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <ul>
+          <li v-for="category in categorise" :key="category.id">
+            {{category.name}}
+          </li>
+        </ul>
+      </div>
 </template>
 
 <script>
+import gql from 'apollo-boost';
 import HelloWorld from '@/components/HelloWorld.vue';
+
+// const queryCategories = gql`
+//   query {
+//     getCategories {
+//       id
+//       name
+//     }
+//   }
+// `;
+
 
 export default {
   name: 'home',
+  data() {
+    return {
+      categorise: [],
+    };
+  },
+  // apollo: {
+  //   categorise: {
+  //     query: queryCategories,
+  //   },
+  // },
   components: {
     HelloWorld,
+  },
+
+  mounted() {
+    console.log(this.$apollo);
+    this.$apolloProvider.defaultClient.query({
+      query: gql`
+        getCategories {
+          id
+          name
+        }
+      `,
+    }).then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      console.log(error);
+    });
   },
 };
 </script>
